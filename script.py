9ac574cf595800b9108ff4f5cba7ce54ed8e1bc4
Borevych Anna <borevychanna@gmail.com>
2026-09-07 21:30:17 +0300

    Add script

---
+++ script.py
@@ -0,0 +1,42 @@
+print('Hello World!')
+
+a=5
+n=8
+b=4.7
+c='tralalelo tralala'
+k=True
+j=False
+lst=[1,2,3,4,5]
+tpl=(1,2,3,4,5,6)
+dct={2: '5',
+     4: '7'}
+st={1,2,3,4,5,6}
+
+print(a)
+print(n)
+print(b)
+print(c)
+print(k)
+print(j)
+print(lst)
+print(tpl)
+print(dct)
+print(st)
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(k))
+print(type(j))
+print(type(lst))
+print(type(tpl))
+print(type(dct))
+print(type(st))
+
+print(a+n)
+print(a-n)
+print(a*n)
+print(a**n)
+print(a/n)
+print(a//n)
+print(a%n)
